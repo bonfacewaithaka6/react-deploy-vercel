ff6cb6b105e14d178018a2556d73e93421d99973
bonface-nayboy <bonnieway615@gmail.com>
2024-06-22 16:56:21 +0300

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bonnie !!! yoh men am bog manh bonniesdgfsh
+          Bonnie !!! yoh men am bog manh bonnie
         </p>
         <a
           className="App-link"
